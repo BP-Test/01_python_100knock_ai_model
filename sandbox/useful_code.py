e8936adba8457c3ecc165e1ddb6ebc67a980fe61
BP-Test <tatsuro.0315@keio.jp>
2022-04-12 17:25:17 +0900

    Add docstring

--- sandbox/useful_code.py
+++ sandbox/useful_code.py
@@ -8,36 +8,54 @@ from termcolor import colored
 from IPython.display import HTML as html_print
 
 
-# Current system path
-pprint(sys.path)
-
-os.chdir(os.path.dirname(os.path.abspath("__file__")))
-__dir__ = os.getcwd()
-pprint(__dir__)
-
-
 # enable cls() on anaconda console
 def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
+    """Enable cls() in python console
 
-
-
+    Returns:
+        _type_: acts as cls(cmd windows) or clear(bash)
+    """
+    return os.system('cls' if os.name=='nt' else 'clear')
 
 # Method that works only on python/cmd console
 init()
-print(colored('Python Programming !', 'green', 'on_red'))
+colored('Python Programming !', 'green', 'on_red')
 
 # Method that works on jupyter notebooks.
 def cstr(s, color='black'):
-    return "<text style=color:{}>{}</text>".format(color, s)
+    """returns a colored text on python console
 
-left, word, right = 'foo' , 'abc' , 'bar'
-display(html_print(cstr(' '.join([left, cstr(word, color='red'), right]), color='black')))
-display(html_print(cstr('abc', 'red')))
-del left, word, right
+    Args:
+        s (str): A string that you want to make it colored.
+        color (str, optional): color that you want to speficy. Defaults to 'black'.
+
+    Returns:
+        _type_: colored str
+    """
+    return "<text style=color:{}>{}</text>".format(color, s)
 
 # Turn jupyter method into a function
 
-def clean_print(string, color='black'):
-    return html_print(cstr(f'{string:}', color))
+def clean_print(string, color='red'):
+    """Prints a colorful print
 
+    Args:
+        string (str): string that you want to print f'' also works.
+        color (str, optional): color that you want to use. Defaults to 'black'.
+
+    Returns:
+        _type_: colored string
+    """
+    clean_output = html_print(cstr(f'{string:}', color))
+    return display(clean_output)
+
+clean_print('You have imported fancy print function!', 'red')
+
+if __name__ == '__main__':
+    init()
+    print(colored('Python Programming !', 'green', 'on_red'))
+    clean_print('You have imported fancy print function!', 'red')
+    left, word, right = 'foo' , 'abc' , 'bar'
+    display(html_print(cstr(' '.join([left, cstr(word, color='red'), right]), color='black')))
+    display(html_print(cstr('abc', 'red')))
+    del left, word, right
